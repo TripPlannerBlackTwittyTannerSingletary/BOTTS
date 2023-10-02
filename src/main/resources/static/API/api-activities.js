9fa709c1e31c6df4d83cab00a1ffc2dfc62d1776
Braden Wittkop <bradenjwittkop@gmail.com>
2023-10-02 12:02:09 -0500

    added tims code and refactoring

--- src/main/resources/static/API/api-activities.js
+++ src/main/resources/static/API/api-activities.js
@@ -28,3 +28,111 @@ async function main() {
 }
 
 document.addEventListener('DOMContentLoaded', main);
+
+
+
+
+
+
+
+
+// tims code
+
+function geocode(search, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
+
+function reverseGeocode(coordinates, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        })
+        // to get all the data from the request, comment out the following three lines...
+        .then(function(data) {
+            return data.features[0].place_name;
+        });
+}
+
+
+
+let citySearch = document.querySelector('#searchBox')
+
+
+function goToInput() {
+    let searchedCity = citySearch.value;
+
+    // Assuming geocode() returns a promise resolving to an array with latitude and longitude
+    geocode(searchedCity, MAPBOX_TOKEN).then((data) => {
+        let lat = data[1];
+        let long = data[0];
+
+        const apiUrlToken = "https://test.api.amadeus.com/v1/security/oauth2/token";
+        const clientId = "jGAH9x2bXMOP1SAMGeocuUWBU0PGgpY4";
+        const clientSecret = "W6QETDbFu24l9gBl";
+
+        const formData = new URLSearchParams();
+        formData.append("grant_type", "client_credentials");
+        formData.append("client_id", clientId);
+        formData.append("client_secret", clientSecret);
+
+        fetch(apiUrlToken, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        })
+            .then(response => response.json())
+            .then(tokenData => {
+                const accessToken = tokenData.access_token;
+
+                const apiUrl = `https://test.api.amadeus.com/v1/shopping/activities?latitude=${lat}&longitude=${long}&radius=21`;
+
+                const headers = {
+                    Authorization: `Bearer ${accessToken}`,
+                };
+
+                fetch(apiUrl, { headers })
+                    .then((response) => {
+                        if (!response.ok) {
+                            throw new Error(`HTTP error! Status: ${response.status}`);
+                        }
+                        return response.json();
+                    })
+                    .then((activityData) => {
+                        // Handle and process the activity data here
+                        console.log(activityData);
+                    })
+                    .catch((error) => {
+                        // Handle errors here
+                        console.error('Error:', error);
+                    });
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    });
+}
+
+document.querySelector('#search-city').addEventListener('click', () => {
+    goToInput();
+})
+
+
+
+
+
+
+
+
+
