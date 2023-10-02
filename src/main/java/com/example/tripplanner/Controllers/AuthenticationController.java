a61b88bd662a21c9a49a28c7f70266555a398ea8
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-10-02 16:33:43 -0500

    completed functionality that logged in user is redirected to profile when trying to log in again

--- src/main/java/com/example/tripplanner/Controllers/AuthenticationController.java
+++ src/main/java/com/example/tripplanner/Controllers/AuthenticationController.java
@@ -1,13 +1,29 @@
 package com.example.tripplanner.Controllers;
 
+import com.example.tripplanner.Models.User;
+import com.example.tripplanner.Repositories.UserRepository;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthenticationController {
+    private UserRepository userDao;
+
+    public AuthenticationController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
     @GetMapping("/login")
     public String showLoginForm(Model model) {
-        return "users/login";
+//        User loggedInUser = userDao.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if ((authentication instanceof AnonymousAuthenticationToken)) {
+            return "users/login";
+        } else
+        return "users/profile";
     }
 }
