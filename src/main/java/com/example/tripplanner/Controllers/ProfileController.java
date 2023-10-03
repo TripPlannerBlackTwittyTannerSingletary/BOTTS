19490c0328c2395ff5ab29e27571b1af590282bc
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-10-03 11:54:00 -0500

    added search entity and associate table

--- src/main/java/com/example/tripplanner/Controllers/ProfileController.java
+++ src/main/java/com/example/tripplanner/Controllers/ProfileController.java
@@ -1,6 +1,9 @@
 package com.example.tripplanner.Controllers;
 
+import com.example.tripplanner.Models.User;
 import com.example.tripplanner.Repositories.TripRepository;
+import com.example.tripplanner.Repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProfileController {
 
     private TripRepository tripsDao;
+    private UserRepository usersDao;
+
+    public ProfileController(TripRepository tripsDao, UserRepository usersDao) {
+        this.tripsDao = tripsDao;
+        this.usersDao = usersDao;
+    }
+
     @GetMapping("")
     public String indexPage(Model model) {
-//        model.addAttribute("trips", tripsDao.findAll());
+        User loggedInUser = usersDao.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+        System.out.println(loggedInUser.getFirstName());
+        model.addAttribute("user", loggedInUser);
         return "users/profile";
     }
 }
