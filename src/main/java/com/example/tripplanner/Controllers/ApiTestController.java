a8a64b32de27bc05a078b7852dee7929f699d663
Braden Wittkop <bradenjwittkop@gmail.com>
2023-10-03 11:56:37 -0500

    added fetch function to get back end data as well edited apiTest controller

--- src/main/java/com/example/tripplanner/Controllers/ApiTestController.java
+++ src/main/java/com/example/tripplanner/Controllers/ApiTestController.java
@@ -1,14 +1,31 @@
 package com.example.tripplanner.Controllers;
 
+import com.example.tripplanner.Models.User;
+import com.example.tripplanner.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 public class ApiTestController {
 
+	UserRepository userDao;
+
+	public ApiTestController(UserRepository userDao) {
+		this.userDao = userDao;
+	}
+
 	@GetMapping("/api-test")
 	public String showApiTest() {
 		return "users/api-test";
+	}
+
+	@GetMapping("/api/users")
+	@ResponseBody
+	public List<User> getAllUsers() {
+		return userDao.findAll();
 	}
 }
