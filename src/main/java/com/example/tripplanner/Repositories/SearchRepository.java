8c71c0bacacff4496776ea8152e7d85a0f808746
Jeremy Tanner <tanner.jeremy.j@gmail.com>
2023-10-05 16:37:47 -0500

    fixed trips in profile

--- src/main/java/com/example/tripplanner/Repositories/SearchRepository.java
+++ src/main/java/com/example/tripplanner/Repositories/SearchRepository.java
@@ -4,5 +4,5 @@ import com.example.tripplanner.Models.Search;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SearchRepository extends JpaRepository<Search, Long> {
-   Search findbySearch(String search);
+//   Search findbySearch(String search);
 }
